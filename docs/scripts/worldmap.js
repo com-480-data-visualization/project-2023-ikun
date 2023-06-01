d651e1c2173b1a367becf33ead3fb8733f4f2aca
zhaobo <zwan6154@uni.sydney.edu.au>
2023-06-01 21:09:38 +0100

    base map

---
+++ docs/scripts/worldmap.js
@@ -0,0 +1,22 @@
+var map = L
+  .map('mapid')
+  .setView([47, 2], 4);   // center position + zoom
+
+// Add a tile to the map = a background. Comes from OpenStreetmap
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
+    maxZoom: 6,
+    minZoom: 2,
+    }).addTo(map);
+
+// Add a svg layer to the map
+L.svg().addTo(map);
+
+function update() {
+  d3.selectAll("circle")
+    .attr("cx", function(d){ return map.latLngToLayerPoint([d.lat, d.long]).x })
+    .attr("cy", function(d){ return map.latLngToLayerPoint([d.lat, d.long]).y })
+}
+
+map.on("moveend", update)
